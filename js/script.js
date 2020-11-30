12ab53e673f4f1aa5b3e1dff067b6495175d3b8a
unknown <example@gmail.com>
2020-11-30 20:58:27 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+"use strict";
+
+
+//4 lesson
+// let number=5;
+// let NUmber=6;
+// const leftBorderWidgth=1;
+// number=10;
+// console.log(number);
+// const obj={
+//     a:50
+// };
+// obj.a=10;
+// console.log(obj);
+// {
+//     let result=50;
+// }
+
+//5-7 lesson
+// let number = 4.6;
+// console.log(-4 / 0);
+// console.log('string' * 9);
+// const persone = "Alex";
+// const bool = true;
+
+//  const obj = {
+//      name: "John",
+//     age: 25,
+//      isMarried: false,
+//  };
+//   console.log(obj.name);
+// // console.log(obj["name"]);
+// let arr=['plum.png', 'orange.png', 6, 'apple.bmp',[],{}];
+// console.log(arr[2]);
+
+
+//alert('Hello');
+// const result=confirm('Are you here?');
+// console.log(result);
+
+// const answer=+prompt('вам есть 18?', '18');
+// console.log(answer+5);
+
+//const answers=[];
+
+// answers[0]=prompt('Как ваше имя?','');
+// answers[1]=prompt('Как ваша фамилия?','');
+// answers[2]=prompt('Сколько вам лет?','');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+//8 lesson
+// const category='toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user='Ivan';
+// alert(`Привет, ${user}`);
+
+
+//9 lesson
+//унврный плюс - это плюс котроый ставится перед чем-то 
+console.log('arr' + '- object');
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// --decr;
+console.log(++incr);//11
+console.log(--decr);//9
+console.log(incr++);//10
+console.log(decr--);//10
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 != 8);
+console.log(2*4 === '8');
+
+const isChecked=true,
+isClose=false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
