133e89a03f6e002793b564ac7530597862a82298
Artem <ind.artem2003@gmail.com>
2020-12-02 15:29:51 +0200

    16 lesson

--- js/script.js
+++ js/script.js
@@ -121,7 +121,7 @@
 //     default:
 //         console.log('не в этот раз');
 //         break;
-    
+
 
 // }
 
@@ -147,6 +147,50 @@
 //         continue;
 //     }
 //     console.log(i);
-    
+
 // }
 
+
+// 16 lesson
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     console.log(num);
+
+
+// }
+
+// showFirstMessage('Hello World!');
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {
+//     let num = 50;
+
+//     //
+
+
+//     return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function () {
+//     console.log('Hello!');
+// };
+
+// logger();
+
+// const calc1 = (a, b) =>a + b;
+// const calc2 = (a, b) =>{
+//     console.log("1");
+//     return a+b;
+// };
