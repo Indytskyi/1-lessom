dad63fa5207485c6dee4cdca87a8c78fe816656a
Artem <ind.artem2003@gmail.com>
2020-12-10 23:38:12 +0200

    22 lesson

--- js/script.js
+++ js/script.js
@@ -339,3 +339,100 @@
 
 // console.log(products.join('; '));
 
+
+//22 lesson
+//Передача по ссылке или по значкению, Spread оператор (ES6-ES9)
+
+// let a = 5,
+// b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a:5,
+//     b:1
+// };
+
+// const copy = obj; //Ссылку
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy (mainObj) {
+//  let objcopy = {};
+
+//  let key;
+//  for(key in mainObj) {
+//      objcopy[key] = mainObj[key];
+//  }
+//  return objcopy;
+// }
+
+// const numbers = {
+//     a:2,
+//     b:5,
+//     c: {
+//         x:7,
+//         y:4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(numbers);
+// console.log(newNumbers);
+
+// const add = {
+//     d: 17,
+//     e: 20,
+
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// const oldArry = ['a', 'b', 'c'];
+// const newArray = oldArry.slice();
+
+// newArray[1] = 'klfwenbf';
+// console.log(newArray);
+// console.log(oldArry);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//     blogs = ['wordpress', 'livejournal', 'blogger'],
+//     internet =[...video, ...blogs, 'vk', 'facebook'];
+
+//     console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ['a', 'b'];
+
+// const newAarray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newQ = {...q};
